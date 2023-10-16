624311f8979d9b8dc9e84b7a0546c035b0f7dd65
liisak-cyber <liisakmodels@gmail.com>
2023-10-16 13:18:27 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,20 +3,21 @@
 /**
  * rev_string - Reverses a string
  * @s: The string to be modified
+ *
  * Return: void
  */
 void rev_string(char *s)
 {
-    int len = 0, index = 0;
-    char tmp;
+	char tmp;
+	int len = 0, index = 0;
 
-    while (s[index++])
-        len++;
+	while (s[index++])
+		len++;
 
-    for (index = len - 1; index >= len / 2; index--)
-    {
-        tmp = s[index];
-        s[index] = s[len - index - 1];
-        s[len - index - 1] = tmp;
-    }
-}    
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+	}
+}
