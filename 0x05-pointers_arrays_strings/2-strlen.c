020a1062fb1291a071473ae8bd3909d8700ca78d
liisak-cyber <liisakmodels@gmail.com>
2023-10-16 11:45:11 -0700

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 /**
- * _srlen - Returns the length of a string.
- * @str: The string to get the length of 
- *Return: The length of the @str.
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of
+ *
+ * Return: The length of the @str.
  */
 size_t _strlen(const char *str)
-
 {
 	size_t length = 0;
 
-	while (*str++)
-	length++;
+	while (*str)
+	{
+		str++;
+		length++;
+	}
 	return (length);
 }
