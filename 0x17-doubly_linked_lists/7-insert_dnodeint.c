4cf0cc1ec4606a109ea6d4fb6d305a8dfba62e51
liisak-cyber <liisakmodels@gmail.com>
2024-01-27 03:09:32 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,52 @@
 #include "lists.h"
 
-/*
- * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *                            list at a given position.
- * @h: A pointer to the head of the dlistint_t list.
- * @idx: The position to insert the new node.
- * @n: The integer for the new node to contain.
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: A pointer to the head of the doubly linked list.
+ * @idx: The index where the new node should be added. Index starts at 0.
+ * @n: The value to be stored in the new node.
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * Description:
+ * This function inserts a new node at the specified index in
+ * a doubly linked list. If successful, it returns the address of the
+ * new node; otherwise, it returns NULL. If not possible to add new node at
+ * the specified index, the function does not modify
+ * the list and returns NULL.
+ *
+ * Return: The address of the new node if successful, otherwise NULL.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *tmp = *h, *new;
+	dlistint_t *tmp = *h, *new;
 
-    /* If the index is 0, insert at the beginning of the list. */
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	/* If the index is 0, insert at the beginning of the list. */
+	if (idx == 0)
+	return (add_dnodeint(h, n));
 
-    /* Traverse the list to reach the desired position. */
-    for (; idx != 1; idx--)
-    {
-        tmp = tmp->next;
-        if (tmp == NULL)
-            return (NULL);
-    }
+	/* Traverse the list to reach the desired position. */
+	for (; idx != 1; idx--)
+	{
+	tmp = tmp->next;
+	if (tmp == NULL)
+	return (NULL);
+	}
 
-    /* If the desired position is at the end, insert at the end of the list. */
-    if (tmp->next == NULL)
-        return (add_dnodeint_end(h, n));
+	/* If the desired position is at the end, insert at the end of the list. */
+	if (tmp->next == NULL)
+	return (add_dnodeint_end(h, n));
 
-    /* Allocate memory for the new node. */
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
+	/* Allocate memory for the new node. */
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	return (NULL);
 
-    /* Set the value of the new node and update the links. */
-    new->n = n;
-    new->prev = tmp;
-    new->next = tmp->next;
-    tmp->next->prev = new;
-    tmp->next = new;
+	/* Set the value of the new node and update the links. */
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-    return (new);
+	return (new);
 }
+
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,47 +1,56 @@
 #include "lists.h"
 
-/*
- * delete_dnodeint_at_index - Deletes a node from a dlistint_t
- *                            at a given index.
+/**
+ * delete_dnodeint_at_index - Deletes node from a dlistint_t at a given index.
  * @head: A pointer to the head of the dlistint_t.
  * @index: The index of the node to delete.
+ *
+ * Description:
+ * This function deletes a node from a doubly linked list at a given index.
+ * If the deletion is successful, it returns 1. If the deletion fails (e.g., if
+ * the index is out of bounds), it returns -1.
  *
  * Return: Upon success - 1.
  *         Otherwise - -1.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp = *head;
+    /* Implementation of the function goes here. */
+}
 
-    /* If the list is empty, return -1 indicating failure. */
-    if (*head == NULL)
-        return (-1);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
 
-    /* Traverse the list to reach the desired index. */
-    for (; index != 0; index--)
-    {
-        if (tmp == NULL)
-            return (-1);
-        tmp = tmp->next;
-    }
+	/* If the list is empty, return -1 indicating failure. */
+	if (*head == NULL)
+	return (-1);
 
-    /* If the node to delete is the head node. */
-    if (tmp == *head)
-    {
-        *head = tmp->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-    }
-    /* If the node to delete is not the head node. */
-    else
-    {
-        tmp->prev->next = tmp->next;
-        if (tmp->next != NULL)
-            tmp->next->prev = tmp->prev;
-    }
+	/* Traverse the list to reach the desired index. */
+	for (; index != 0; index--)
+	{
+	if (tmp == NULL)
+	return (-1);
+	tmp = tmp->next;
+	}
 
-    /* Free the memory of the deleted node. */
-    free(tmp);
-    return (1);
+	/* If the node to delete is the head node. */
+	if (tmp == *head)
+	{
+	*head = tmp->next;
+	if (*head != NULL)
+	(*head)->prev = NULL;
+	}
+	/* If the node to delete is not the head node. */
+	else
+	{
+	tmp->prev->next = tmp->next;
+	if (tmp->next != NULL)
+	tmp->next->prev = tmp->prev;
+	}
+
+	/* Free the memory of the deleted node. */
+	free(tmp);
+	return (1);
 }
 
