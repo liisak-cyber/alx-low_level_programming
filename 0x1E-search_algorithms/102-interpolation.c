5ce54eda72a275abdd9960807f0c90a0cbbe22c4
liisak-cyber <liisakmodels@gmail.com>
2024-07-13 16:31:05 -0700

    Compiling Task 9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,21 +12,22 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low, high, pos;
-
 	if (array == NULL || size == 0)
-		return -1;
+		return (-1);
+
+	size_t low, high, pos;
 
 	low = 0;
 	high = size - 1;
 
-	while (low <= high && value >= array[low] && value <= array[high]) {
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		pos = low + (value - array[low]) * (high - low) / (array[high] - array[low]);
 
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
-			return pos;
+			return (pos);
 
 		if (array[pos] < value)
 			low = pos + 1;
@@ -35,6 +36,6 @@ int interpolation_search(int *array, size_t size, int value)
 	}
 
 	printf("Value checked array[%lu] is out of range\n", low);
-	/* Value not found */
-	return -1;
+
+	return (-1); /* Value not found */
 }
