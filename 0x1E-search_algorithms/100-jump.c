33d68b1f731741d25c32003a631209e583f50acc
liisak-cyber <liisakmodels@gmail.com>
2024-07-13 15:53:16 -0700

    Compiling Task 0 and 1

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,15 +13,16 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t step = sqrt(size); // Jump step
-    size_t prev = 0; // Previous step
+    size_t step = sqrt(size); /* Jump step */
+    size_t prev = 0; /* Previous step */
+    size_t i; /* Loop variable */
 
     if (array == NULL || size == 0)
         return -1;
 
     printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 
-    // Jump through the array
+    /* Jump through the array */
     while (array[step] < value && step < size) {
         prev = step;
         printf("Value checked array[%lu] = [%d]\n", step, array[step]);
@@ -32,13 +33,12 @@ int jump_search(int *array, size_t size, int value)
 
     printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
 
-    // Perform linear search in the found range
-    for (size_t i = prev; i <= step && i < size; ++i) {
+    /* Perform linear search in the found range */
+    for (i = prev; i <= step && i < size; ++i) {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         if (array[i] == value)
             return i;
     }
 
-    return -1; // Value not found
+    return -1; /* Value not found */
 }
-
