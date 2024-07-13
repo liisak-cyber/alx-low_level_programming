76c1aee24493a88a0e71139e067d05b5f40aeebc
liisak-cyber <liisakmodels@gmail.com>
2024-07-13 16:07:46 -0700

    Compiling Task 9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,27 +12,29 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    if (array == NULL || size == 0)
-        return -1;
+	size_t low, high, pos;
 
-    size_t low = 0;
-    size_t high = size - 1;
+	if (array == NULL || size == 0)
+		return -1;
 
-    while (low <= high && value >= array[low] && value <= array[high]) {
-        size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+	low = 0;
+	high = size - 1;
 
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+	while (low <= high && value >= array[low] && value <= array[high]) {
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
-        if (array[pos] == value)
-            return pos;
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-        if (array[pos] < value)
-            low = pos + 1;
-        else
-            high = pos - 1;
-    }
+		if (array[pos] == value)
+			return pos;
 
-    printf("Value checked array[%lu] is out of range\n", low);
-    return -1; // Value not found
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+
+	printf("Value checked array[%lu] is out of range\n", low);
+	/* Value not found */
+	return -1;
 }
-
